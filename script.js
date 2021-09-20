0b5123f484e03da694f00b71bf94a2f6debb505d
“ashrhea1986” <ashleyrhea1986@yahoo.com>
2021-09-20 16:05:20 -0500

    Completed JSBasicsExercise.

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log(7);
+// Numbers Strings Booleans Null Undefined
+// + - * / ** %
+console.log(5%4); console.log(5%2);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Ashley ".length + "Boston-Gutierrez".length);
+console.log("Avengers:Endgame"[15]);
+// Bonus
+// 1.
+console.log("\\");
+// 2.
+console.log((5**4)-9);
+// 3.
+console.log(("I".length**"Am".length)-"Iron".length+"Man".length);
+// 4.
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STARLORD"[0]);
+// 5.
+var m=25
+var c=4
+var u=m+c 
+console.log(u);
